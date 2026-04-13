6e6e07932d0e8fb990156e0884dac3e79b357d8f
Atul Sagar <atulsagar55@gmail.com>
2026-04-13 16:20:37 +0530

    Added DTO

--- src/main/java/com/example/userservice/controller/UserController.java
+++ src/main/java/com/example/userservice/controller/UserController.java
@@ -1,7 +1,10 @@
 package com.example.userservice.controller;
 
+import com.example.userservice.dto.UserRequestDTO;
+import com.example.userservice.dto.UserResponseDTO;
 import com.example.userservice.entity.User;
 import com.example.userservice.service.UserService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,12 +20,44 @@ public class UserController {
     }
 
     @PostMapping
-    public User CreateUser(@RequestBody User user){
-        return userService.saveUser(user);
+    public ResponseEntity<?> CreateUser(@RequestBody UserRequestDTO request){
+
+        if (request.getName() == null || request.getName().isEmpty()) {
+            return ResponseEntity.badRequest().body("Name is required");
+        }
+
+        if (request.getEmail() == null || request.getEmail().isEmpty()) {
+            return ResponseEntity.badRequest().body("Email is required");
+        }
+
+        User user = new User();
+        user.setName(request.getName());
+        user.setEmail(request.getEmail());
+
+        User savedUser = userService.saveUser(user);
+
+        UserResponseDTO response = new UserResponseDTO(
+                savedUser.getId(),
+                savedUser.getName(),
+                savedUser.getEmail()
+        );
+
+        return ResponseEntity.ok(response);
+
     }
 
     @GetMapping
-    public List<User> getUsers(){
-        return userService.getAllUsers();
+    public List<UserResponseDTO> getUsers(){
+
+        return userService.getAllUsers()
+                .stream()
+                .map(user -> new UserResponseDTO(
+                        user.getId(),
+                        user.getName(),
+                        user.getEmail()
+                ))
+                .toList();
     }
+
+
 }
